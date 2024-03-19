30cd53e21ffb34c00a4714e7819256024ee82799
Liam Stoldt <liamstoldt@yahoo.com>
2024-03-20 09:15:11 +1030

    feat: inital module setup

---
+++ pkmn/pokemon.py
@@ -0,0 +1,42 @@
+"""Module and class for pokemon handling"""
+
+
+class Stats:
+    """Stats object for holding standard pokemon stats"""
+
+    def __init__(self):
+        self.max_hp: int = 0
+        self.attack: int = 0
+        self.defence: int = 0
+        self.s_attack: int = 0
+        self.s_defence: int = 0
+        self.speed: int = 0
+
+
+class Pokemon:
+    """The pokmeon class"""
+
+    def __init__(self):
+        """Initialise all starting variables"""
+
+        # organise stats
+        self.stats = Stats()
+        self.hp: int = 0
+
+        # moveset
+        self.moves = [None, None, None, None]
+
+        # status conditions
+        self.status_condition = None
+
+        # xp and levelling
+        self.xp: int = 0
+        self.xp_to_next_level: int = 0
+        self.level: int = 1
+        self.level_to_evolution: int = None
+        self.evolution_pokemon: Pokemon = None
+
+    def take_damage(self, amount: int):
+        """Make this pokemon take damage"""
+
+        self.hp -= amount
